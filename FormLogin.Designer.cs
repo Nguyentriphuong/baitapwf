745ffef28e1315f44f0c48e58ff363afb8ae969f
Nguyentriphuong <Nguyentriphuong2107@gmail.com>
2019-01-08 05:38:07 +0700

    first

---
+++ FormLogin.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace newwf
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelId = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.Label();
+            this.textId = new System.Windows.Forms.TextBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Location = new System.Drawing.Point(45, 77);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(19, 17);
+            this.labelId.TabIndex = 0;
+            this.labelId.Text = "Id";
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(45, 113);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(45, 17);
+            this.name.TabIndex = 1;
+            this.name.Text = "Name";
+            // 
+            // textId
+            // 
+            this.textId.Location = new System.Drawing.Point(171, 74);
+            this.textId.Name = "textId";
+            this.textId.Size = new System.Drawing.Size(179, 22);
+            this.textId.TabIndex = 2;
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(171, 113);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(172, 22);
+            this.textName.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.textId);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.labelId);
+            this.Name = "FormLogin";
+            this.Text = "FormLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.TextBox textId;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ FormLogin.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace newwf
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                if (!(textId.Text == string.Empty))
+                {
+                    if (!(textName.Text == string.Empty))
+                    {
+                        String str = "Data Source=DESKTOP-KFQPSKO;Initial Catalog=Baitap;Integrated Security=True";
+                        String query = "SELECT * FROM Login where Id = '" + textId.Text + "'and NameId = '" + this.textName.Text + "'";
+                        SqlConnection con = new SqlConnection(str);
+                        SqlCommand cmd = new SqlCommand(query, con);
+                        SqlDataReader db;
+                        con.Open();
+                        db = cmd.ExecuteReader();
+                        int count = 0;
+                        while (db.Read())
+                        {
+                            count = count + 1;
+                        }
+
+                        if (count == 1)
+                        {
+                            MessageBox.Show("username and password is correct");
+                        }
+
+                        else if (count > 1)
+                        {
+                            MessageBox.Show("Duplicate username and password", "login page");
+                        }
+
+                        else
+                        {
+                            MessageBox.Show(" username and password incorrect", "login page");
+                        }
+
+                    }
+                    else MessageBox.Show("plesse fill in Name");
+                }
+                else MessageBox.Show("Pleses fill in Id");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+       
+    }
+}
